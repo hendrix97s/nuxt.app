211011c39abe37d530c37270ca0a10df9fc6dbad
Luiz F. Lima <lf.system@outlook.com>
2022-07-06 13:31:00 -0300

    Added image size

--- pages/index.vue
+++ pages/index.vue
@@ -32,9 +32,12 @@ export default Vue.extend({
         { hid: 'og:title', property: 'og:title', content: 'Employees - Tests using open graph' },
         { hid: 'og:description', property: 'og:description', content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
         { hid: 'og:image', property: 'og:image', content: this.image},
+        { hid: 'og:image:type', property: 'og:image:type', content: 'image/jpeg'},
+        { hid: 'og:image:width', property: 'og:image:width', content: '1200'},
+        { hid: 'og:image:height', property: 'og:image:height', content: '630'},
         { hid: 'og:type', property: 'og:type', content: 'website' },
         { hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt example with Vercel' },
-        { hid: 'og:url', property: 'og:url', content: 'https://doc-hendrix97s.vercel.app/' },
+        { hid: 'og:url', property: 'og:url', content: 'https://nuxt-study.vercel.app/' },
         { hid: 'og:locale', property: 'og:locale', content: 'pt' },
       ]
     }
